1030bb3a256ec8afe94eb3a8c40335c20d5509a8
d2ckbum <rlagmlqja96@gmail.com>
2025-07-01 17:40:06 +0900

    first commit

---
+++ src/main/java/kr/co/sist/HotelMimirApplication.java
@@ -0,0 +1,13 @@
+package kr.co.sist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelMimirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelMimirApplication.class, args);
+	}
+
+}
