1d874a78c3812a41eab64551847f623523f85c61
JHyun12864 <hyeon12864@naver.com>
2025-07-07 13:53:37 +0900

    2025 07 07 월 오후 1:53 현지영 - MainController.java 이름 중복 오류 수정

--- src/main/java/kr/co/sist/administrator/controller/MainController.java
+++ src/main/java/kr/co/sist/administrator/controller/MainController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 	
-	@GetMapping("/")
-	public String index() {
-		return "index";
-	}//index
-	
 	
 	@GetMapping("/admin")
 	public String admin() {
