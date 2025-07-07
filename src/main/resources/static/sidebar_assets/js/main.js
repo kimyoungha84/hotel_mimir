234e444eaf019004b0d8f3cd5d52e4a742f95637
JHyun12864 <hyeon12864@naver.com>
2025-07-07 17:52:21 +0900

    2025 07 07 월 오후 5:50 - 사이드바 그냥 고정형태로 변경

--- src/main/resources/static/sidebar_assets/js/main.js
+++ src/main/resources/static/sidebar_assets/js/main.js
@@ -1,6 +1,6 @@
 $(function(){
 
-    showMenu('nav-toggle', 'navbar', 'body-pd');
+    //showMenu('nav-toggle', 'navbar', 'body-pd');
 
     /* LINK ACTIVE */
     const linkColor = document.querySelectorAll('.nav__link');
@@ -21,20 +21,50 @@ $(function(){
 
     /* COLLAPSE MENU */
     const linkCollapse = document.getElementsByClassName('collapse__link');
+    
+
     var i;
 
     for(i=0;i<linkCollapse.length;i++) {
         linkCollapse[i].addEventListener('click', function(){
-            //debugger; //여기서 linkCollapse 내에 뭐가 있는지 알아야 한단 말이지....
-            //alert("linkCollapse 길이-----"+linkCollapse.length);
+            /*모든 메뉴 접기*/
+            const allMenus = document.querySelectorAll('.collapse__menu');
+                allMenus.forEach(function(menu) {
+                menu.classList.remove('showCollapse');
+            });//allMenus
+
+            const allArrows = document.querySelectorAll('.collapse__link .arrow-icon');
+                allArrows.forEach(function(icon) {
+                icon.classList.remove('rotate');
+            });
 
             //감싼거 보여줘, 안보이게 해줘
-            const collapseMenu = this.nextElementSibling;
-            collapseMenu.classList.toggle('showCollapse');//showCollapse라는 속성을 추가/삭제 (on/off하는 것처럼 추가/삭제)
+            // const collapseMenu = this.nextElementSibling;
+            // collapseMenu.classList.toggle('showCollapse');//showCollapse라는 속성을 추가/삭제 (on/off하는 것처럼 추가/삭제)
+            
 
-            //화살표일거임.
-            const rotate = collapseMenu.previousElementSibling;
-            rotate.classList.toggle('rotate');
+
+            // //화살표일거임.
+            // const rotate = collapseMenu.previousElementSibling;
+            // rotate.classList.toggle('rotate');
+
+        /*클릭한 메뉴만 열기*/
+        const collapseMenu = this.nextElementSibling;
+        const arrowIcon = this.querySelector('.arrow-icon');
+
+        const isOpen = collapseMenu.classList.contains('showCollapse');
+
+        if (!isOpen) {
+            collapseMenu.classList.add('showCollapse');
+            if (arrowIcon) {
+                arrowIcon.classList.add('rotate');
+            }
+        } else {
+            collapseMenu.classList.remove('showCollapse');
+            if (arrowIcon) {
+                arrowIcon.classList.remove('rotate');
+            }
+        }
         });
     }//end for
 
@@ -50,18 +80,16 @@ function showMenu(toggleId, navbarId, bodyId) {
     const toggle = document.getElementById(toggleId),
     navbar = document.getElementById(navbarId),
     bodypadding = document.getElementById(bodyId);
-    // alert("toggle======="+toggle);
-    // alert("navbar======="+navbar);
+    
+    /*버튼을 클릭했을 때 toggle*/
     if( toggle && navbar ) {
         toggle.addEventListener('click', function(){
-            //메뉴바가 열릴 때, true
-            //메뉴바가 닫힐 때, false
-            // debugger;
-            //alert(bodypadding);
-            navbar.classList.toggle('expander');
-            bodypadding.classList.toggle('shrink');
+            const isNowExpanded = navbar.classList.toggle("expander");
+            bodypadding.classList.toggle("shrink");
 
-        })
+
+
+        });
     }//end if
 }//function showMenu
 
