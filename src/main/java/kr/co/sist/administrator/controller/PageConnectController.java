28df10cf3410b29f74cff887b0e28d938f373fbc
JHyun12864 <hyeon12864@naver.com>
2025-07-07 13:57:11 +0900

    2025 07 07 월 오후 1:56 현지영 - Controller 충돌 오류 해결

--- src/main/java/kr/co/sist/administrator/controller/PageConnectController.java
+++ src/main/java/kr/co/sist/administrator/controller/PageConnectController.java
@@ -28,60 +28,60 @@ public class PageConnectController {
 	
 	
 	/*************************************/
-	/*객실 관리*/
-	@GetMapping("/admin/room")
-	public String roomManage() {
-		return "";
-	}//roomManage
-	//객실 정보
-	@GetMapping("/admin/roomInfo")
-	public String roomInfo() {
-		return "";
-	}//roomInfo
-	//예약 현황
-	@GetMapping("/admin/roomReserve")
-	public String roomReservation() {
-		return "";
-	}//roomReservation
-	//매출 현황
-	@GetMapping("/admin/roomSales")
-	public String roomSalesStatus() {
-		return "";
-	}//roomSalesStatus
-	
-	/*************************************/
-	/*다이닝 관리*/
-	@GetMapping("/admin/dinning")
-	public String dinningManage() {
-		return "";
-	}//dinningManage
-	//예약 관리
-	@GetMapping("/admin/dinningReserve")
-	public String dinningReserve() {
-		return "";
-	}//dinningReserve
-	
-	
-	/*************************************/
-	/*문의 관리*/
-	@GetMapping("/admin/inquiry")
-	public String inquiryManage() {
-		return "";
-	}//inquiryManage
-	//1:1 문의
-	@GetMapping("/admin/personalManage")
-	public String personalManage() {
-		return "";
-	}//personalManage
-	//FAQ
-	@GetMapping("/admin/faq")
-	public String faq() {
-		return "";
-	}//faq
-	//실시간 문의
-	@GetMapping("/admin/realtime")
-	public String realTimeInquiry() {
-		return "";
-	}//realTimeInquiry
-		
+//	/*객실 관리*/
+//	@GetMapping("/admin/room")
+//	public String roomManage() {
+//		return "";
+//	}//roomManage
+//	//객실 정보
+//	@GetMapping("/admin/roomInfo")
+//	public String roomInfo() {
+//		return "";
+//	}//roomInfo
+//	//예약 현황
+//	@GetMapping("/admin/roomReserve")
+//	public String roomReservation() {
+//		return "";
+//	}//roomReservation
+//	//매출 현황
+//	@GetMapping("/admin/roomSales")
+//	public String roomSalesStatus() {
+//		return "";
+//	}//roomSalesStatus
+//	
+//	/*************************************/
+//	/*다이닝 관리*/
+//	@GetMapping("/admin/dinning")
+//	public String dinningManage() {
+//		return "";
+//	}//dinningManage
+//	//예약 관리
+//	@GetMapping("/admin/dinningReserve")
+//	public String dinningReserve() {
+//		return "";
+//	}//dinningReserve
+//	
+//	
+//	/*************************************/
+//	/*문의 관리*/
+//	@GetMapping("/admin/inquiry")
+//	public String inquiryManage() {
+//		return "";
+//	}//inquiryManage
+//	//1:1 문의
+//	@GetMapping("/admin/personalManage")
+//	public String personalManage() {
+//		return "";
+//	}//personalManage
+//	//FAQ
+//	@GetMapping("/admin/faq")
+//	public String faq() {
+//		return "";
+//	}//faq
+//	//실시간 문의
+//	@GetMapping("/admin/realtime")
+//	public String realTimeInquiry() {
+//		return "";
+//	}//realTimeInquiry
+//		
 }//class
