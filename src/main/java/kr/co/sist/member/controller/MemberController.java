d892c49064a23a927322a87766e8d77a4a84087b
d2ckbum <rlagmlqja96@gmail.com>
2025-07-07 17:45:48 +0900

    smtp 설 정 추 가

--- src/main/java/kr/co/sist/member/controller/MemberController.java
+++ src/main/java/kr/co/sist/member/controller/MemberController.java
@@ -1,7 +1,10 @@
 package kr.co.sist.member.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import kr.co.sist.member.dto.MemberDTO;
 
 @Controller
 public class MemberController {
@@ -12,9 +15,17 @@ public class MemberController {
 	
 	//회원가입 폼 호출
 	@GetMapping("/member/registerFrm")
-	public String registerFrm() {
+	public String registerFrm(Model model) {
+		model.addAttribute("memberDTO", new MemberDTO());
 		
 		return "member/registerFrm";
+	}
+	
+	//이메일 인증 팝업 호출
+	@GetMapping("/member/email-popup")
+	public String emailPopup() {
+		
+		return "member/email_popup";
 	}
 	
 	
