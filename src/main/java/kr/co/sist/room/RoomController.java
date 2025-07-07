d3fc79f077040ac5489e68aaa39da1a21ecb56cc
jinwon052 <coco3924@naver.com>
2025-07-07 15:51:14 +0900

    room commit 0707

--- src/main/java/kr/co/sist/room/RoomController.java
+++ src/main/java/kr/co/sist/room/RoomController.java
@@ -5,10 +5,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RoomController {
+	
+	@GetMapping("roomhome")
+	public String roomHome() {
+		return "room/roomHome";
+	}//roomList
 
 	@GetMapping("roomlist")
 	public String roomList() {
 		return "room/room_list";
+	}//roomList
+	
+	
+	@GetMapping("roomdetail")
+	public String roomDetail() {
+		return "room/roomDetail";
 	}//roomList
 	
 	
