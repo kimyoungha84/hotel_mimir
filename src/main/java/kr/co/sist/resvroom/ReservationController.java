3cfac2bb57970dcb64a147ef90c9acad7f8c98a4
jinwon052 <coco3924@naver.com>
2025-07-07 15:39:11 +0900

    room commit 0707

--- src/main/java/kr/co/sist/resvroom/ReservationController.java
+++ src/main/java/kr/co/sist/resvroom/ReservationController.java
@@ -1,4 +1,4 @@
-package kr.co.sist.resv;
+package kr.co.sist.resvroom;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/kr/co/sist/room/RoomController.java
+++ src/main/java/kr/co/sist/room/RoomController.java
@@ -11,4 +11,5 @@ public class RoomController {
 		return "room/room_list";
 	}//roomList
 	
+	
 }//class
