ba5ce6926d5d588d6deae2e72ddc5d270ecd582c
jaein2740 <dntjd2740@gmail.com>
2025-07-11 14:52:15 +0900

    0711 14:25

--- src/main/java/kr/co/sist/resvroom/ReservationController.java
+++ src/main/java/kr/co/sist/resvroom/ReservationController.java
@@ -1,14 +1,32 @@
 package kr.co.sist.resvroom;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ReservationController {
 
-	@GetMapping("resvRoom")
-	public String roomList() {
-		return "room_resv/resvRoom";
+	@GetMapping("/room_resv/resvRoom")
+
+	public String roomList(@RequestParam("roomId") int roomId,
+		    @RequestParam("checkIn") String checkIn,
+		    @RequestParam("checkOut") String checkOut,
+		    @RequestParam("adult") int adult,
+		    @RequestParam("child") int child,
+		    @RequestParam("typeName") String typeName,
+		    Model model) {
+		
+		 model.addAttribute("roomId", roomId);
+		    model.addAttribute("checkIn", checkIn);
+		    model.addAttribute("checkOut", checkOut);
+		    model.addAttribute("adult", adult);
+		    model.addAttribute("child", child);
+		    model.addAttribute("typeName", typeName);
+
+		    return "room_resv/resvRoom"; // 보여줄 페이지 경로
+
 	}//roomList
 	
 	@GetMapping("searchResv")
